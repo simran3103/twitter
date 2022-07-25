cd5319ab456d64714b03267594b2bfbff2f33ef3
simran3103 <simrannegi17@gmail.com>
2022-07-25 20:02:43 +0530

    new changes for upload file

--- src/components/Hheader/Hheader.tsx
+++ src/components/Hheader/Hheader.tsx
@@ -5,10 +5,41 @@ import { useState } from 'react'
 
 const Hheader = () => {
     const [text, setText] = useState<string | undefined>('')
+    const [url, setUrl] = useState<string | undefined>('')
 
     const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value)
     }
+
+    const imageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files![0];
+        console.log('files', event.target.files![0])
+        // const name = file.name
+        // console.log(name)
+        setUrl(file.name)
+        getBase64(file).then(base64 => {
+            localStorage["fileBase64"] = base64;
+            console.debug("file stored", base64);
+            console.log('base64', base64)
+        });
+    }
+
+    const getBase64 = (file: any) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                console.log('reader result', reader)
+                // setUrl(reader.result.data)
+                resolve(reader.result)
+            };
+            reader.onerror = error => {
+                console.log('reader err', error)
+                reject(error)
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+
     return (
         <div className="head">
             <div className='head-1'>
@@ -16,9 +47,13 @@ const Hheader = () => {
                 <img src={star} alt='star' />
             </div>
             <div className='head-2'>
-                <img src={pro} alt='user_profile' style={{ height: '2rem', width: '2rem', borderRadius: '2rem' }} />
+                <img src={url} alt='user_profile' style={{ height: '2rem', width: '2rem', borderRadius: '2rem' }} />
                 <input type="text" value={text} placeholder="What's happening?" onChange={(e) => changeHandler(e)} />
             </div >
+            <div>
+                <label htmlFor="imageFile">Select file</label>
+                <input type="file" id="imageFile" name="imgFile" onChange={imageUpload} />
+            </div>
         </div>
     )
 }
